c8942f9a75f79ce33a71871699d4ec3891f886a2
exec zsh <fsuhas@gmx.com>
2022-05-24 00:21:55 +0200

    add typed.js no finish

---
+++ app/javascript/controllers/typed.js
@@ -0,0 +1,7 @@
+// Can also be included with a regular script tag
+import Typed from 'typed.js';
+
+var typed = new Typed('.element', {
+  strings: ["First sentence.", "Second sentence."],
+  typeSpeed: 30
+});
