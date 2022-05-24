36eb3d3d4054db8605b02336b497c82b1328c54f
Alain Noel <alain.noel34@gmail.com>
2022-05-24 15:14:59 +0200

    seeds en cours

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,27 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#   movies = Movie.create([{ email: "Star Wars" }, { email: "Lord of the Rings" }])
+#   Character.create(email: "Luke", movie: movies.first)
+require 'open-uri'
+require 'json'
+
+User.create(email: 'pierre@gmail.com', password: '1234')
+
+User.create(email: 'sarah@gmail.com', password: '1234')
+
+User.create(email: 'nicolas@gmail.com', password: '1234')
+
+User.create(email: 'louis@gmail.com', password: '1234')
+
+User.create(email: 'juliette@gmail.com', password: '1234')
+
+Teacher.create(feature: '', price: '30', picture: '')
+
+Teacher.create(feature: '', price: '40', picture: '')
+
+Teacher.create(feature: '', price: '60', picture: '')
+
+Teacher.create(feature: '', price: '70', picture: '')
+
+Teacher.create(feature: '', price: '100', picture: '')
