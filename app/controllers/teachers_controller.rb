9169d3be02725e391df4c071417f368e69c5b184
Livya Branco <livyabranco@gmail.com>
2022-05-24 12:22:55 +0200

    teacher controller and user schema is update

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,22 +1,31 @@
 class TeachersController < ApplicationController
-
   def index
+    @teachers = Teacher.all
   end
 
   def new
+    @teacher = Teacher.new
+    authorize @teacher
+  end
+
+  def edit
+    @teacher = Teacher.find(params[:id])
+    @teacher = Teacher.new
+    authorize @teacher
   end
 
   def create
     @teacher = Teacher.new(teacher_params)
     @teacher.user = current_user
+    authorize @teacher
   end
 
   def show
+    authorize @teachers
+    @teacher = Teacher.find(params[:id])
   end
 
   def teacher_params
-    params.require(:teacher).permit(:name, :city, :image_url)
+    params.require(:teacher).permit(:first_name, :last_name, :city, :picture, :features)
   end
-
-
 end
