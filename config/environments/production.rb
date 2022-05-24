9a761ba577bbfc7d638e13e3aa5077f8e7c7b856
Alain Noel <alain.noel34@gmail.com>
2022-05-24 17:46:05 +0200

    testlol

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  # coucou c'est moi
   # Code is not reloaded between requests.
   config.cache_classes = true
 
