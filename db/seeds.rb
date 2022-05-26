f8af62a9110360ed47115659ccd0f8d665002564
exec zsh <fsuhas@gmx.com>
2022-05-26 17:21:04 +0200

    seed and link navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 require 'open-uri'
 require 'json'
 
+
 puts "Start seed"
 puts "User seed start..."
 
@@ -25,24 +26,24 @@ puts "user seed finish !"
 puts "Teacher seed start..."
 
 piere = Teacher.new(feature: 'Red wine specialist with demonstrated skills on Bugundy wines. Also expert in cider due to my roots.', price: '30', picture: '', user: pierre)
-piere.photo.attach(io: file, filename: "pierre.png", content_type: "image/png")
-piere.save
+piere.photo.attach(io: URI.open("https://source.unsplash.com/random"), filename: "pierre.png", content_type: "image/png")
+piere.save!
 
 sara = Teacher.new(feature: 'White wine from France. Fond of vins du Languedoc. Can also provide advise on rhums', user: sarah)
-sara.photo.attach(io: file, filename: "sarah.png", content_type: "image/png")
-sara.save
+sara.photo.attach(io: URI.open("https://source.unsplash.com/random"), filename: "sarah.png", content_type: "image/png")
+sara.save!
 
 nico = Teacher.new(feature: 'White wine from France. Fond of vins du Languedoc. Can also provide advise on rhums', price: '60', picture: '', user: nicolas)
-nico.photo.attach(io: file, filename: "nicolas.png", content_type: "image/png")
-nico.save
+nico.photo.attach(io: URI.open("https://source.unsplash.com/random"), filename: "nicolas.png", content_type: "image/png")
+nico.save!
 
-lui = Teacher.new(feature: 'Red wine specialist with demonstrated skills on Bugundy wines. Also expert in cider due to my roots', user: louis)
-loui.photo.attach(io: file, filename: "louis.png", content_type: "image/png")
-loui.save
+loui = Teacher.new(feature: 'Red wine specialist with demonstrated skills on Bugundy wines. Also expert in cider due to my roots', user: louis)
+loui.photo.attach(io: URI.open("https://source.unsplash.com/random"), filename: "louis.png", content_type: "image/png")
+loui.save!
 
 julie = Teacher.new(feature: 'White wine from France. Fond of vins du Languedoc. Can also provide advise on rhums', user: juliette)
-julie.photo.attach(io: file, filename: "julie.png", content_type: "image/png")
-julie.save
+julie.photo.attach(io: URI.open("https://source.unsplash.com/random"), filename: "julie.png", content_type: "image/png")
+julie.save!
 
 puts "teacher seed finish !"
 
@@ -50,7 +51,6 @@ puts "Booking seed start..."
 
 Booking.create(date: '25 mai', user_id: pierre.id, teacher_id: piere.id )
 Booking.create(date: '25 mai', user_id: sarah.id, teacher_id: sara.id )
-Booking.create(date: '25 mai', user_id: nicolas.id, teac<<<<<<< seedher_id: nico.id )
+Booking.create(date: '25 mai', user_id: nicolas.id, teacher_id: nico.id )
 
 puts "Booking seed finish !"
-
