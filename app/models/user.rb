a02c88ad2758612fa04f472f2477336ee1cb9f39
Alain Noel <alain.noel34@gmail.com>
2022-05-26 17:38:22 +0200

    teachershow

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :teachers
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  geocoded_by :city
+  after_validation :geocode, if: :will_save_change_to_city?
 end
