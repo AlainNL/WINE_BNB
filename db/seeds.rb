5f150e1c88b06a9f0547fcb1261fa32fbe1cae9b
Emmanuelle Amouroux <amouroux.emmanuelle@orange.fr>
2022-05-26 15:16:00 +0200

    Booking and seed enhancements

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,34 +10,36 @@ require 'json'
 
 puts "Start seed"
 
+Booking.destroy_all
 Teacher.destroy_all
 User.destroy_all
 
 
-pierre = User.create(email: 'pierre@gmail.com', password: '12345678', first_name: "pierre", last_name: "jean")
+pierre = User.create(email: 'pierre@gmail.com', password: '12345678', first_name: "pierre", last_name: "jean", city: "Rennes")
 
-sarah = User.create(email: 'sarah@gmail.com', password: '12345678', first_name: "sarah", last_name: "du desert")
+sarah = User.create(email: 'sarah@gmail.com', password: '12345678', first_name: "sarah", last_name: "du desert", city: "Paris")
 
-nicolas = User.create(email: 'nicolas@gmail.com', password: '12345678', first_name: "nicolas", last_name: "petit")
+nicolas = User.create(email: 'nicolas@gmail.com', password: '12345678', first_name: "nicolas", last_name: "petit", city: "Bordeaux")
 
-louis = User.create(email: 'louis@gmail.com', password: '12345678', first_name: "louis", last_name: "lafonte")
+louis = User.create(email: 'louis@gmail.com', password: '12345678', first_name: "louis", last_name: "lafonte", city: "Paris")
 
-juliette = User.create(email: 'juliette@gmail.com', password: '12345678', first_name: "juliette", last_name: "Jetaime")
+juliette = User.create(email: 'juliette@gmail.com', password: '12345678', first_name: "juliette", last_name: "Jetaime", city: "Lille")
 
 puts "user seed"
 
-piere = Teacher.create(feature: '', price: '30', picture: 'https://media.istockphoto.com/photos/portrait-of-mature-businessman-picture-id1327603929?k=20&m=1327603929&s=612x612&w=0&h=UgF0xUZXJbNVshou9Jpt_bKtQRIa9IyXEkxlAIZxyK8=', user: pierre)
+piere = Teacher.create(feature: 'Red wine specialist with demonstrated skills on Bugundy wines. Also expert in cider due to my roots.', price: '30', picture: 'https://media.istockphoto.com/photos/portrait-of-mature-businessman-picture-id1327603929?k=20&m=1327603929&s=612x612&w=0&h=UgF0xUZXJbNVshou9Jpt_bKtQRIa9IyXEkxlAIZxyK8=', user: pierre)
 
-sara = Teacher.create(feature: '', price: '40', picture: 'https://media.istockphoto.com/photos/young-african-woman-smiling-at-sunset-picture-id969233490?k=20&m=969233490&s=612x612&w=0&h=jXLgjbu0CIWEohHgcx_ZHBhrcH3hNKcg7TVJgehndPg=', user: sarah)
+sara = Teacher.create(feature: 'Beers from all over the world with a focus on Belgian beers', price: '40', picture: 'https://media.istockphoto.com/photos/young-african-woman-smiling-at-sunset-picture-id969233490?k=20&m=969233490&s=612x612&w=0&h=jXLgjbu0CIWEohHgcx_ZHBhrcH3hNKcg7TVJgehndPg=', user: sarah)
 
-nico = Teacher.create(feature: '', price: '60', picture: 'https://media.istockphoto.com/photos/smiling-man-with-arms-crossed-wearing-eyeglasses-picture-id1284027512?k=20&m=1284027512&s=612x612&w=0&h=gJFKUHjcUwv-WHAl6zLHmbfsJmJTLp5p1KqFBqSsO3w=', user: nicolas)
+nico = Teacher.create(feature: 'White wine from France. Fond of vins du Languedoc. Can also provide advise on rhums', price: '60', picture: 'https://media.istockphoto.com/photos/smiling-man-with-arms-crossed-wearing-eyeglasses-picture-id1284027512?k=20&m=1284027512&s=612x612&w=0&h=gJFKUHjcUwv-WHAl6zLHmbfsJmJTLp5p1KqFBqSsO3w=', user: nicolas)
 
-Teacher.create(feature: '', price: '70', picture: 'https://media.istockphoto.com/photos/damn-right-i-approve-picture-id1139070467?k=20&m=1139070467&s=612x612&w=0&h=uYQ3Xxsq_cqiLELXnmfTTME4ghXwsfsGXTjtb9UyOyo=', user: louis)
+Teacher.create(feature: 'Whiskies from Scotland are my favorite beverage. I acn also share my passion about Bordeaux wines', price: '70', picture: 'https://media.istockphoto.com/photos/damn-right-i-approve-picture-id1139070467?k=20&m=1139070467&s=612x612&w=0&h=uYQ3Xxsq_cqiLELXnmfTTME4ghXwsfsGXTjtb9UyOyo=', user: louis)
 
-Teacher.create(feature: '', price: '100', picture: 'https://media.istockphoto.com/photos/portrait-of-young-smiling-woman-face-partially-covered-with-flying-picture-id1297159365?k=20&m=1297159365&s=612x612&w=0&h=RHC5Qy40yAgwmpZVj0vXKUzcUm6qrY2MSSWzj2nLQlg=', user: juliette)
+Teacher.create(feature: 'Very fond of wines from South Afrika where I used to be oenologist', price: '100', picture: 'https://media.istockphoto.com/photos/portrait-of-young-smiling-woman-face-partially-covered-with-flying-picture-id1297159365?k=20&m=1297159365&s=612x612&w=0&h=RHC5Qy40yAgwmpZVj0vXKUzcUm6qrY2MSSWzj2nLQlg=', user: juliette)
 
 Booking.create(date: '25 mai', user_id: pierre.id, teacher_id: piere.id )
 Booking.create(date: '25 mai', user_id: sarah.id, teacher_id: sara.id )
 Booking.create(date: '25 mai', user_id: nicolas.id, teacher_id: nico.id )
 
 puts "teacher seed"
+puts "Booking seed"
